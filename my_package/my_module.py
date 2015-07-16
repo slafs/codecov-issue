df1eb6a12e9da608f1bf4b042af90cdd0115a189
Sławek Ehlert <slafs@op.pl>
2015-07-16 15:03:38 +0200

    add demonstration functions and one test

---
+++ my_package/my_module.py
@@ -0,0 +1,11 @@
+# encoding: utf-8
+from __future__ import print_function
+
+def my_function(a):
+    print('invoking my function')
+    return a
+
+def my_other_function(b):
+    print('invoking my other function')
+    return b
+
